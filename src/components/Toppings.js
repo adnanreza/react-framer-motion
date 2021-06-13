3feef99d92dd3431c998068b5f7e1251b64c8bf9
Adnan Reza <adnan.reza@outlook.com>
2021-06-13 15:15:05 -0700

    Added variants to Order and Toppings

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -2,11 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion'
 
+const containerVariants = {
+  hidden: {
+    x: '100vw',
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { 
+      type: 'spring', delay: 0.5
+    }
+  }
+}
+
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
 
   return (
-    <div className="toppings container">
+    <motion.div className="toppings container"
+    variants={containerVariants}
+    initial="hidden"
+    animate="visible"
+    >
       
       <h3>Step 2: Choose Toppings</h3>
       <ul>
@@ -19,7 +37,7 @@ const Toppings = ({ addTopping, pizza }) => {
               originX: 0,
               color: '#f8e112',
              }}
-             transition={{ type:'spring', stiffness: 300}}
+              transition={{ type:'spring', stiffness: 300}}
               key={topping} onClick={() => addTopping(topping)}>
               <span className={spanClass}>{ topping }</span>
             </motion.li>
@@ -36,7 +54,7 @@ const Toppings = ({ addTopping, pizza }) => {
         </motion.button>
       </Link>
 
-    </div>
+    </motion.div>
   )
 }
 
